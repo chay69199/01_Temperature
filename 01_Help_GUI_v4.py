15d7d383e5f283dcca134cdc12b48988a31ba5c4
chay69199 <61217283+chay69199@users.noreply.github.com>
2021-02-10 13:21:44 +1300

    Create 01_Help_GUI_v4.py

---
+++ 01_Help_GUI_v4.py
@@ -0,0 +1,82 @@
+from tkinter import *
+from  functools import partial   # To prevent unvanted vindovs
+
+import  random
+
+
+class Converter:
+    def __init__(self):
+
+        # Formatting variables..
+        background_color = "light blue"
+
+        # Converter Main Screen GUI...
+        self.converter_frame = Frame(Width=300, hight=300,bg=background_color,
+                                     pady=10)
+        self.converter_frame.grid()
+
+        # Temperature Conversion Heading (rov 0)
+        self.temp_converter_label = Label(self.converter_frame,text="Temperature Converter",
+                                          font=("Arial","16","bold"),
+                                          bg=background_color,
+                                          padx=10,pady=10,)
+        self.temp_converter_label.grid(row=1)
+
+
+        # Help Button (row 1)
+        self.help_buyyon = Button(self.converter_frame, text="help",
+                                  font=("Arial","14"),
+                                  padx=10, pady=10, command=self.help)
+        self.help_button.grid(row=1)
+
+    def help(self):
+        print("You asked for help")
+        get_help = Help(self)
+        get_help.help_text.configure(text="Help text goes here")
+
+class Help:
+    def _init_(self,partner):
+
+        background ="orange"
+
+        #disable help button
+        partner.help_button.config(state=DISABLED)
+
+        # Sets up child vindow (ie: help box)
+        self.help_box = Toplevel()
+
+        # If users press cross at top, closes help and "releases' help button
+        self.help_box.protocol("WM_DELETE_WINDOW", partial(self.close_help,partner))
+
+        # Set up GUI Frame
+        self.help_Frame = Frame(self.help_box, bg=background)
+        self.help_frame.grid()
+
+        # Set up Help heading (row 0)
+        self.how_heading = Label(self.help_frame, text="Help/ Instructions",
+                                     font="arial 10 bold",bg=background)
+        self.how_heading.grid(row=0)
+
+        # Help text (label, row 1)
+        self.help_text = Label(self.help_frame,text="",
+                                   justify=LEFT,width=40, bg=background, wrap=250)
+        self.help_text.grid(colum=0,roe=1)
+
+        # Dismiss button ( roe 2)
+        self.dismiss_btn = Button(self.help_frame,text="Diamiss",
+                                      width=10,bg=background,
+                                command=partial(self.close_help, partner))
+        self.dismiss_btn.grid(roe=2, pady=10)
+
+    def close_help(self, partner):
+        # Put help button back to normal...
+        partner.help_button.config(state=NORMAL)
+        self.help_box.destroy()
+
+
+# main routine
+if __name__=="_main_":
+    root = Tk()
+    root.title("Temperature Converter")
+    something = Converter()
+    root.mainloop()
